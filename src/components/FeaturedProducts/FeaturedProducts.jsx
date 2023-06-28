754ffa48a57b92d60e7edc736915b8e928480e21
Your Namekshai-7 <akshai2537@gmail.com>
2023-06-28 19:50:45 +0530

    first commit

---
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,9 @@
+import "./FeaturedProducts.scss";
+
+const FeaturedProducts = () => {
+  return (
+    <div className="featuredProducts">FeaturedProducts</div>
+  )
+}
+
+export default FeaturedProducts
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,10 +1,13 @@
 import "./Home.scss";
 import Slider from '../../components/Slider/Slider';
+import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 
 const Home = () => {
   return (
     <div className='home'>
-      <Slider/>
+      <Slider />
+      <FeaturedProducts type="featured"/>
+      <FeaturedProducts type="trending"/>
     </div>
   )
 }
